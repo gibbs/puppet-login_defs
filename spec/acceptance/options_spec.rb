ab32a0bf2778bd0d0aa5dc3ca7abd1e5d693b237
Dan Gibbs <daniel.gibbs@gmail.com>
2022-03-02 23:13:29 +0000

    Group content match tests

--- spec/acceptance/options_spec.rb
+++ spec/acceptance/options_spec.rb
@@ -37,17 +37,19 @@ describe 'options' do
     it { is_expected.to be_owned_by 'root' }
     it { is_expected.to be_readable }
 
-    its(:content) { is_expected.to match(%r{MANAGED BY PUPPET}) }
+    its(:content) do
+      is_expected.to match(%r{MANAGED BY PUPPET})
 
-    # Check custom content
-    its(:content) { is_expected.to match(%r{GID_MIN	2000}) }
-    its(:content) { is_expected.to match(%r{#HUSHLOGIN_FILE	.hushlogin}) }
-    its(:content) { is_expected.to match(%r{USERGROUPS_ENAB	no}) }
-    its(:content) { is_expected.to match(%r{PASS_MAX_DAYS	123}) }
-    its(:content) { is_expected.to match(%r{# testcomment}) }
-    its(:content) { is_expected.to match(%r{#DEFAULT_HOME	yes}) }
+      # Custom
+      is_expected.to match(%r{GID_MIN	2000})
+      is_expected.to match(%r{#HUSHLOGIN_FILE	.hushlogin})
+      is_expected.to match(%r{USERGROUPS_ENAB	no})
+      is_expected.to match(%r{PASS_MAX_DAYS	123})
+      is_expected.to match(%r{# testcomment})
+      is_expected.to match(%r{#DEFAULT_HOME	yes})
 
-    # Ensure a common value is correct
-    its(:content) { is_expected.to match(%r{ENCRYPT_METHOD	SHA512}) }
+      # Ensure a common value is correct
+      is_expected.to match(%r{ENCRYPT_METHOD	SHA512})
+    end
   end
 end
